170dabb5dafc8f824ed6fbaceba814c44e4f26de
Rithik1611 <rithik161104@gmail.com>
2024-08-20 10:28:25 +0530

    profile ui

--- lib/app/app_pallete.dart
+++ lib/app/app_pallete.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppPallete {
-  static const Color primary = Color.fromRGBO(103, 65, 136, 0);
-  static const Color primary2 = Color.fromRGBO(195, 172, 208, 0);
-  static const Color secondary = Color.fromRGBO(255, 251, 245, 0);
-  static const Color secondary2 = Color.fromRGBO(247, 239, 229, 0);
+  static const Color primary = Color.fromRGBO(103, 65, 136, 1);
+  static const Color primary2 = Color.fromRGBO(195, 172, 208, 1);
+  static const Color secondary = Color.fromRGBO(255, 251, 245, 1);
+  static const Color secondary2 = Color.fromRGBO(247, 239, 229, 1);
 }
--- lib/view/next_page.dart
+++ lib/view/next_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:team_up/app/app_pallete.dart';
 import 'package:team_up/models/student.dart';
 
 class NextPage extends StatelessWidget {
@@ -12,21 +13,95 @@ class NextPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Next Page"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Name: ${student.name}"),
-            Text("Department: ${student.department}"),
-            Text("Section: ${student.section}"),
-            Text("Year: ${student.year}"),
-            Text("Skills: ${student.skills.join(', ')}"),
-            Text("Interests: ${student.interests.join(', ')}"),
-            student.profilePic.path.isNotEmpty
-                ? Image.file(student.profilePic, height: 100)
-                : const Text('No profile picture selected.'),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: student.profilePic.path.isNotEmpty
+                        ? FileImage(student.profilePic)
+                        : AssetImage('assets/profile_image.png')
+                            as ImageProvider,
+                    backgroundColor: Colors.blue,
+                  ),
+                  SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        student.name,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '${student.year} Year',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                      Text(
+                        '${student.department}',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                      Text(
+                        '${student.section}',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Skill set:',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ),
+              SizedBox(height: 10),
+              Wrap(
+                spacing: 8.0,
+                children: student.skills
+                    .map((skill) => Chip(
+                        label:
+                            Text(skill, style: TextStyle(color: Colors.white)),
+                        backgroundColor: AppPallete.primary))
+                    .toList(),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Interested:',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ),
+              SizedBox(height: 10),
+              Wrap(
+                spacing: 8.0,
+                children: student.interests
+                    .map((interest) => Chip(
+                        label: Text(interest,
+                            style: TextStyle(color: Colors.white)),
+                        backgroundColor: AppPallete.primary))
+                    .toList(),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
